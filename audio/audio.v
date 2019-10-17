78bf17c92412847c2d5ac8a53c294f4d4d663047
Elliot Williams <elliot@pre.postero.us>
2019-10-17 22:27:28 +0200

    phasing saws demo of DAC
    works great.  Sounds fine.

--- audio/audio.v
+++ audio/audio.v
@@ -2,47 +2,35 @@ module top(
 	input clk, 
 	input [7:0] btn, 
 	output [5:0] led, 
-	output pwmout, 
-	output [5:0] sao1 
+	output [5:0] sao1,
+	output pwmout
 );
 /* Button and audio workout */
 
-
-reg [23:0] counter;
-reg audio;
-reg [15:0] overflow;
-wire [3:0] button;
-
-button_number button_number_1(
-	.clk (clk), 
-	.btn (btn), 
-	.button (button)
+dac mydac (
+	.clk (clk),
+	.pcm (mix),
+	.out (pwmout)
 );
 
+reg [23:0] counter = 0;
+reg [15:0] ramp1 = 0; 
+reg [15:0] ramp2 = 0; 
+wire [15:0] mix; 
 
-// Chromatic divisors of 8 MHz, starting with C3
-// 15289, 14431, 13621, 12856, 12135, 11454, 10811, 10204, 9631, 9091, 8581, 8099
-
+/* Set up divider counter */
 always @(posedge clk) begin 
 	counter <= counter + 1;
-	case(button)
-		0: overflow <= 15289;
-		1: overflow <= 13621;
-		2: overflow <= 12135;
-		3: overflow <= 11454;
-		4: overflow <= 10204;
-		5: overflow <= 9091;
-		6: overflow <= 8099;
-		7: overflow <= 15289/2;
-		default: overflow <= 0;
-	endcase
-
-	if (counter > overflow)
-		counter <= 0;
-	audio <= counter[12];
 end
 
-assign led[6:0] = overflow[6:0];
-assign pwmout = audio;
+// this value controls the frequency
+always @(posedge counter[7]) begin 
+	ramp1 <= ramp1 + 108; // six-hundred step ramp waveform
+end
+always @(posedge counter[7]) begin 
+	ramp2 <= ramp2 + 109; // six-hundred step ramp waveform
+end
+
+assign mix = ramp1/2 + ramp2/2;
 
 endmodule
