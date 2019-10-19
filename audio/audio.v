03f32bbe0d66a8a1b4592c685c49429012cff7b1
Elliot Williams <elliot@pre.postero.us>
2019-10-19 21:35:46 +0200

    pushbutton saw demo
    but this is a dead end.  need to move to full-on DDS style

--- audio/audio.v
+++ audio/audio.v
@@ -13,24 +13,41 @@ dac mydac (
 	.out (pwmout)
 );
 
-reg [23:0] counter = 0;
-reg [15:0] ramp1 = 0; 
-reg [15:0] ramp2 = 0; 
-wire [15:0] mix; 
+reg [23:0] counter;
+reg [23:0] freq ;
+reg [15:0] mix; 
+
+wire [3:0] button;
+
+button_number my_button_number(
+	.clk (clk), 
+	.btn (btn), 
+	.button (button)
+);
 
 /* Set up divider counter */
 always @(posedge clk) begin 
 	counter <= counter + 1;
+	case (button)
+		0: begin freq <= 30578; end
+		1: begin freq <= 27242; end
+		2: begin freq <= 24270; end
+		3: begin freq <= 22908; end
+		4: begin freq <= 20408; end
+		5: begin freq <= 18182; end
+		6: begin freq <= 16198; end
+		7: begin freq <= 30578/2; end
+		default: begin freq <= 30578; end
+	endcase
+
+	if (counter > freq) begin // this value controls the frequency
+		counter <= 0;
+	end
 end
 
-// this value controls the frequency
-always @(posedge counter[7]) begin 
-	ramp1 <= ramp1 + 108; // six-hundred step ramp waveform
-end
-always @(posedge counter[7]) begin 
-	ramp2 <= ramp2 + 109; // six-hundred step ramp waveform
-end
+// [30578.0, 28862.0, 27242.0, 25713.0, 24270.0, 22908.0, 21622.0, 20408.0, 19263.0, 18182.0, 17161.0, 16198.0]
 
-assign mix = ramp1/2 + ramp2/2;
-
+assign mix = counter;
+assign led[3:0] = button;
 endmodule
+
