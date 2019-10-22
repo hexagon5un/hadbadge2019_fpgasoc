3ab2d81e5a0c6dad861b014307d8282ede7ae2ed
Elliot Williams <elliot@pre.postero.us>
2019-10-22 13:48:47 +0200

    square -> suboctave square
    just adds so much more color, y'know?

--- audio/oscillator.v
+++ audio/oscillator.v
@@ -12,24 +12,27 @@ module oscillator
 );
 localparam TOPBIT = BITDEPTH+BITFRACTION-1;
 localparam PULSEWIDTH = 2**(BITDEPTH-4);
+localparam MIDPOINT = 2**(BITDEPTH-1)-1;
 
 reg [TOPBIT:0] accumulator = 0 ;  
 always @(posedge sample_clock) begin 
 	accumulator <= accumulator + increment;
 end
 
-wire [BITDEPTH-1:0] saw;
+wire [BITDEPTH-1:0] saw ;
 wire [BITDEPTH-1:0] triangle;
 wire [BITDEPTH-1:0] square; 
-wire [BITDEPTH-1:0] pulse;
+wire [BITDEPTH-1:0] pulse ;
 
 
 always @(posedge sample_clock) begin 
 	saw <= accumulator[TOPBIT -: BITDEPTH] ; 
 	triangle <= accumulator[TOPBIT] ? ~accumulator[TOPBIT-1 -: BITDEPTH] : accumulator[TOPBIT-1 -: BITDEPTH];
-	square <= accumulator[TOPBIT] ? 2**BITDEPTH-1 : 0 ;
+        
 	pulse <= accumulator[TOPBIT -: BITDEPTH] < PULSEWIDTH ? 2**BITDEPTH-1 : 0 ;
 end
+always @(posedge accumulator[TOPBIT]) // suboctave square is so much cooler
+  square <= ~square;
 
 reg [1:0] gain;
 reg [2:0] num_ones = voice_select[0] + voice_select[1] + voice_select[2] + voice_select[3];
@@ -40,7 +43,7 @@ always @(posedge sample_clock) begin
 		2: out <= voice_select[0]*saw/2 + voice_select[1]*triangle/2 + voice_select[2] *square/2 + voice_select[3]*pulse/2;
 		3: out <= voice_select[0]*saw/4 + voice_select[1]*triangle/4 + voice_select[2] *square/4 + voice_select[3]*pulse/4;
 		4: out <= voice_select[0]*saw/4 + voice_select[1]*triangle/4 + voice_select[2] *square/4 + voice_select[3]*pulse/4;
-		default: out <= 2**(BITDEPTH-1)-1; // center
+		default: out <= MIDPOINT; 
 	endcase
 end
 
