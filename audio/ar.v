76d08da99d64f46e203564b4368d4eb5b2385a17
Elliot Williams <elliot@pre.postero.us>
2019-10-24 15:35:10 +0200

    push combo logic out to amplifier module
    I'll need this later, for sure.
    Plus it's nice from a modular synth
    perspective.

--- audio/ar.v
+++ audio/ar.v
@@ -1,7 +1,9 @@
 `include "amplifier.v"
 `include "envelope.v"
 
-module ar (
+module ar #(
+	parameter BITDEPTH = 14
+)(
 	input sample_clock,
 	input [BITDEPTH-1:0] in,
 	input [7:0] envelope_attack,
@@ -10,18 +12,10 @@ module ar (
 	output [BITDEPTH-1:0] out
 );
 
-localparam BITDEPTH    = 14;
-localparam BITFRACTION = 6;
-localparam SAMPLECLOCK_DIV = 8;
-localparam SAMPLEFREQ  = 8000000 / 2**SAMPLECLOCK_DIV;  // 31,250 Hz or 32 us
-
-localparam VOLBITS  = 8;
-localparam OFFSET  = 2**(BITDEPTH -1); // idles at mid-volume
-
+// DANGER: if change bit depth, need to pass that info on to submodules
 
 wire [7:0] volume;
-envelope myenv
-(
+envelope myenv (
 	.sample_clock(sample_clock),
 	.gate(gate),
 	.a(envelope_attack),
@@ -29,11 +23,10 @@ envelope myenv
 	.volume(volume) // returns
 );
 
-// Amplifier here.
-wire [BITDEPTH+VOLBITS-1:0] summer;
-wire [BITDEPTH+VOLBITS-1:0] offset;
-assign offset = (2**VOLBITS - volume)*OFFSET;
-assign summer = in * volume + offset;
-assign out = summer[BITDEPTH+VOLBITS-1 -: BITDEPTH] ;
+amplifier myamp (
+	.in(in),
+	.volume(volume),
+	.out(out)
+);
 
 endmodule
